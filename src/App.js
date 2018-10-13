59bb62929dc1f8475b7fa0850d109ff0c46ae5d5
JLangley753 <42213489+JLangley753@users.noreply.github.com>
2018-10-13 11:58:45 +0100

    Created noughts and crosses game mechanism and added basic styling
    Added scoring system, functioning noughts and crosses game. Added restart button

--- src/App.js
+++ src/App.js
@@ -1,42 +1,17 @@
 import React, { Component } from 'react';
 import './App.css';
-
-import Header from './components/header/Header';
-import { Board } from './components/board/Board';
-import { Square } from './components/square/Square';
+import Board from './components/board/board';
 
 class App extends Component {
-  constructor(props) {
-    super();
-    this.state = {
-      turn: 0
-    }
-  }
-  updateTurn() {
-    this.setState({
-      turn: this.state.turn +1
-    })
-  }
-
   render() {
     return (
-      <div id="App">
-        <Header>
+      <div className="app" id="App">
+        <div className="header">
           <h1>Noughts and Crosses</h1>
-        </Header>
-
-        <Board>
-          <Square onClick={this.updateTurn.bind(this)}>{this.state.symbol}</Square>
-          <Square></Square>
-          <Square></Square>
-          <Square></Square>
-          <Square></Square>
-          <Square></Square>
-          <Square></Square>
-          <Square></Square>
-          <Square></Square>
-        </Board>
-
+        </div>
+        <div className="board-wrap">
+          <Board />
+        </div>
       </div>
     )
   }
--- src/components/board/Board.js
+++ src/components/board/Board.js
@@ -1,22 +1,115 @@
-import React from 'react';
-import BoardComponent from './Board.styles';
+import React, {Component} from 'react';
+import './board.css'
 
-export class Board extends React.Component {
+function Square(props) {
+  return (<div className={props.class} onClick={props.onClick}>
+    {props.value}
+  </div>);
+}
+
+class Board extends Component {
   constructor(props) {
     super(props);
-    this.handler = this.handler.bind(this)
+    this.state = {
+      squares: Array(9).fill(null),
+      xCurrentTurn: true,
+      class: 'square',
+    };
   }
-  handler(e) {
-    e.preventDefault()
+
+  handleClick(i) {
+    const squares = this.state.squares.slice();
+    if (calculateWinner(squares) || squares[i]) {
+      return;
+    }
+    squares[i] = this.state.xCurrentTurn
+      ? 'O'
+      : 'X';
     this.setState({
-      current: +1
-    })
+      squares: squares,
+      xCurrentTurn: !this.state.xCurrentTurn,
+    });
+}
+
+restartBoard() {
+  return (this.setState({squares: Array(9).fill(null), xCurrentTurn: true}))
+}
+
+renderSquare(i) {
+  return <Square value={this.state.squares[i]} class={this.state.class} onClick={() => this.handleClick(i)}/>;
+}
+
+render() {
+  const winner = calculateWinner(this.state.squares);
+  let status;
+  if (winner) {
+    status = 'Winner: ' + winner;
+  } else {
+    status = 'Current Player: ' + (
+      this.state.xCurrentTurn
+      ? 'O'
+      : 'X');
   }
-  render() {
-    return (
-    <BoardComponent handler = {this.handler}>
-        {this.props.children}
-      </BoardComponent>
-    )
+
+  return (<div className="board">
+    <div className="status">{status}</div>
+    <div className="board-row">
+      {this.renderSquare(0)}
+      {this.renderSquare(1)}
+      {this.renderSquare(2)}
+    </div>
+    <div className="board-row">
+      {this.renderSquare(3)}
+      {this.renderSquare(4)}
+      {this.renderSquare(5)}
+    </div>
+    <div className="board-row">
+      {this.renderSquare(6)}
+      {this.renderSquare(7)}
+      {this.renderSquare(8)}
+    </div>
+    <div className="button-wrap">
+      <button onClick={this.restartBoard.bind(this)} className="restart-button">Click me to restart</button>
+    </div>
+  </div>)
+}
+}
+
+function calculateWinner(squares) {
+var j;
+const lines = [
+  [
+    0, 1, 2
+  ],
+  [
+    3, 4, 5
+  ],
+  [
+    6, 7, 8
+  ],
+  [
+    0, 3, 6
+  ],
+  [
+    1, 4, 7
+  ],
+  [
+    2, 5, 8
+  ],
+  [
+    0, 4, 8
+  ],
+  [
+    2, 4, 6
+  ]
+];
+for (j = 0; j < lines.length; j++) {
+  const [a, b, c] = lines[j];
+  if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+    return squares[a];
   }
 }
+return null;
+}
+
+export default Board;
